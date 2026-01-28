0fb30e7e401722393c4c3ccf42454ffb163cbbb6
Nathan Cline <nathancline123@gmail.com>
2026-01-27 23:42:49 -0500

    working on caching system

--- internal/pokecache/cache.go
+++ internal/pokecache/cache.go
@@ -3,6 +3,7 @@ package pokecache
 import (
 	"sync"
 	"time"
+
 )
 
 // start of cache that will be imported into our pokedex
@@ -13,6 +14,61 @@ type cacheEntry struct {
 
 // mutex to protect. Read&write at same time can cause crashing in program
 type Cache struct {
-	mu    sync.Mutex
-	items map[string]cacheEntry
+	mu 			sync.Mutex
+	items 		map[string]cacheEntry
+	interval 	time.Duration
+}
+
+
+func NewCache(interval time.Duration) *Cache {
+	
+
+	cache := &Cache{items: make(map[string]cacheEntry),
+					interval: interval}
+
+		go cache.reapLoop()
+
+
+	return cache
+}
+
+func (cache *Cache) Add(key string,val []byte){
+  cache.mu.Lock()
+
+  defer cache.mu.Unlock()
+
+
+	cache.items[key] = cacheEntry{createdAt: time.Now(), val: val}
+	
+
+}
+
+func (cache *Cache) Get(key string) ([]byte, bool){
+	cache.mu.Lock()
+
+	defer cache.mu.Unlock()
+  
+
+	entry, found := cache.items[key]
+
+	if !found {
+		return nil, false
+	}
+
+  return entry.val, true
+
+}
+
+func (cache *Cache) reapLoop() {
+
+	cache.mu.Lock()
+
+	defer cache.mu.Unlock()
+
+	for key, entry := range cache.items{
+	if time.Since(entry.createdAt)>cache.interval{
+		delete(cache.items, key)
+	}
+}
+
 }
